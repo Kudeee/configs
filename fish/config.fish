5962b62208d5b76c1f45b77b2e45bf8d01a64777
kudeee <codysu08@gmail.com>
2026-04-25 12:02:21 +0800

    kitty config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set --universal nvm_default_version lts # or whatever version you use
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
