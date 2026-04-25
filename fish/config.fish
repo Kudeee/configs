c9b7914abbd5125a1362c697cab940dfecda489c
kudeee <codysu08@gmail.com>
2026-04-25 14:10:36 +0800

    kitty config

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,14 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set --universal nvm_default_version lts # or whatever version you use
 
+set -gx EDITOR nvim
+
+printf "\e[2 q"
+
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
+function fish_greeting
+    fastfetch
+end
 #    # smth smth
 #end
